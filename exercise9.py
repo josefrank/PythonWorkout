a26d2dab137098405045ed7f51cbe0dd38f3e0f2
José Frank Hernández <josefrank@users.noreply.github.com>
2021-01-06 09:27:52 -0400

    Create exercise9.py

---
+++ exercise9.py
@@ -0,0 +1,18 @@
+"""
+The bottom line of this exercise is the idea creating a function able to
+retrieve data from a sequence (list, tuple, string) and return a another sequence of
+the same type. This is possible thanks to the idea of slicing [a:b] and wouldn't be
+possible just accessing the elements of the via index [a].
+"""
+
+
+def first_last(sequence):
+    return sequence[:1] + sequence[-1:]
+
+
+numbers = (2, 5, 6, 8, 1)
+
+result = first_last(numbers)
+
+print(result)
+print(type(result))
