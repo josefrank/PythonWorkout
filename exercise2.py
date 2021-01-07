fef8ce05d17e8e57df3886dca0d2906bdf6ab937
José Frank Hernández <josefrank@users.noreply.github.com>
2021-01-07 09:43:08 -0400

    Update exercise2.py

--- exercise2.py
+++ exercise2.py
@@ -1,4 +1,15 @@
-# Write a sum() like function.
+"""
+Write a sum() like function with the feature of taking more than a sequence.
+
+Note: The splat operator [as in mySum(*numbers)] always returns a tuple. If attempting
+to past a sequence (list, tuple, string) as an argument in the function call of MySum()
+will result in a type error because the *numbers parameter will convert the sequence
+into a tuple whose first element would be the sequence itself that wound not be possible
+to be added to the total variable because it is an integer.
+
+The solution to this problem is to add the splat (*) operator in the function call too which
+results in the argument passed to be "unpacked" as it would have been individual values.
+"""
 
 
 def mySum(*numbers):
@@ -11,4 +22,7 @@ def mySum(*numbers):
 myList = [3, 5, 6, 6, 1]
 
 result = mySum(*myList)
+print(result)
+
+result = mySum(3, 5, 6, 6, 1)
 print(result)
